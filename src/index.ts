38e02b458617f98540ef491237f6617c8d534eee
liuyingbin1922 <212164693@qq.com>
2022-06-12 16:12:43 +0800

    feat: 添加typedoc

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,15 @@ import {unitChange} from './utils/index';
 const title = "this is the ts starter";
 const num = '124';
 
+export function getVersion () : string {
+    return '1.0.0';
+}
+
+
+
 
 console.log('num' , unitChange(num));
 console.log('title', title);
+
+
+
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
-export function unitChange(num: string) : number {
+type T = string | number;
+
+export function unitChange(num: T) : T {
     return Number(num) / 100;
 }
